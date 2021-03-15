04747aa8d5c0b077170983eb6ecfaa81435c7ace
35167 <35167@studentwsei.pl>
2021-03-15 12:25:31 +0100

    Wyświetlanie tytulow dziala

--- src/pl/lublin/wsei/java/cwiczenia/Controller.java
+++ src/pl/lublin/wsei/java/cwiczenia/Controller.java
@@ -1,7 +1,10 @@
 package pl.lublin.wsei.java.cwiczenia;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.stage.FileChooser;
 import javafx.fxml.FXML;
 import java.io.File;
@@ -10,6 +13,9 @@ import javax.swing.*;
 
 public class Controller {
     public Label lbFile;
+    public ListView lstInfografiki;
+    ObservableList<String> tytuly = FXCollections.observableArrayList();
+    GusInfoGraphicList igList;
     FileChooser fileChooser= new FileChooser();
     FileChooser.ExtensionFilter xmlFilter = new FileChooser.ExtensionFilter("Pliki XML (*.xml)","*.xml");
 
@@ -21,7 +27,10 @@ public class Controller {
     public void btnOpenFileAction(ActionEvent actionEvent) {
         File file = fileChooser.showOpenDialog(null);
         if (file != null) {
+            igList = new GusInfoGraphicList(file.getAbsolutePath());
             lbFile.setText(file.getAbsolutePath());
+            for (infografika ig: igList.infografiki) tytuly.add(ig.tytul);
+            lstInfografiki.setItems(tytuly);
         }else{
             lbFile.setText("Proszę wczytać plik ...");
         }
